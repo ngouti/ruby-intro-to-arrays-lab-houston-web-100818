beb1c6fdca1771a7ccbb20308e5ae90f1920c03e
Emma Gouti <goutiemma@gmail.com>
2018-10-01 08:11:44 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -9,11 +9,13 @@ def array_with_two_elements
 end
 
 def first_element(array)
-  return array.first()
+  return array.[0]
 end
 
 def third_element(array)
   return array[2]
 end
 
-def last_element
+def last_element(array)
+  return array[-1]
+end
