471f67af3387b16c62bda6e5aa2cdc8a1aa9c86b
Emma Gouti <goutiemma@gmail.com>
2018-10-01 08:11:10 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -3,3 +3,17 @@ def instantiate_new_array
   return array
 end
 
+def array_with_two_elements
+  array = [1,2]
+  return array
+end
+
+def first_element(array)
+  return array.first()
+end
+
+def third_element(array)
+  return array[2]
+end
+
+def last_
